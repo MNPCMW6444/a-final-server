3fed6249e3b732ef09902b04678a68547edff425
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-02 12:25:21 +0300

    mock added

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import sslRedirect from "heroku-ssl-redirect";
 import express from "express";
 import cors from "cors";
+import mock from "./mock.json";
+
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(sslRedirect());
+app.use(express.json());
+
 app.listen(port, () => console.log(`Server started on port: ${port}`));
 
 app.use(
@@ -15,5 +19,5 @@ app.use(
 );
 
 app.get("/all", (req, res) => {
-  res.josn({ asd: "asd" });
+  res.json(mock);
 });
