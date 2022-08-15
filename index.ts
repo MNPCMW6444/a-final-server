ea593544296ab2176e7a7d5ea571af4909e83f92
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-15 13:41:28 +0300

    yes

--- index.ts
+++ index.ts
@@ -11,10 +11,7 @@ const port = process.env.PORT || 5001;
 
 app.use(express.json());
 
-app.listen(
-  port,
-  () => /* console.log(`Server started on port: ${port}`) */ null
-);
+app.listen(port, () => console.log(`Server started on port: ${port}`));
 
 app.use(
   cors({
