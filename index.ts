c2b5a70919b73ee0d7ca076ecc0d26c4c7b93da9
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-17 16:45:53 +0300

    meir

--- index.ts
+++ index.ts
@@ -36,12 +36,12 @@ mongoose.connect(
 
 app.get("/tasks-all", async (_, res) => {
   // res.json({ tasks: mock.tasks, events: [] });
-  res.json({ tasks: await Task.find() });
+  res.json(await Task.find());
 });
 
 app.get("/events-all", async (_, res) => {
   // res.json({ events: mock.events, tasks: [] });
-  res.json({ events: await Event.find() });
+  res.json(await Event.find());
 });
 
 app.get("/all-today", async (_, res) => {
@@ -60,10 +60,10 @@ app.get("/all-today", async (_, res) => {
     now.getMonth(),
     now.getDate()
   );
-  res.json({
-    tasks: await Task.find({ estimatedTime: { $gte: todayBeginnig } }),
-    events: await Event.find({ beginningTime: { $gte: todayBeginnig } }),
-  });
+  res.json([
+    ...(await Task.find({ estimatedTime: { $gte: todayBeginnig } })),
+    ...(await Event.find({ beginningTime: { $gte: todayBeginnig } })),
+  ]);
 });
 
 app.post(
@@ -234,4 +234,17 @@ app.delete("/tasks/:id", async (req, res) => {
   const task = await Task.findById(req.params.id);
   if (task) await task.delete();
   res.json(await Task.find());
+});
+
+app.delete("/item/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (task) await task.delete();
+    res.json(await Task.find());
+  } catch (e) {
+    console.log("so this item is an event......... interesting...");
+  }
+  const event = await Event.findById(req.params.id);
+  if (event) await event.delete();
+  res.json(await Event.find());
 });
