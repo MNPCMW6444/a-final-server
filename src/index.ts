ea9440c330deb1e7629eda45ae40fdb097093e74
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-11-28 16:54:10 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -60,15 +60,3 @@ server.start().then(() => {
     console.log(`Server is now running on http://localhost:${PORT}/graphql`);
   });
 });
-
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
-
-app.post("save", async (req, res) => {
-  console.log(req);
-  res.send();
-});
