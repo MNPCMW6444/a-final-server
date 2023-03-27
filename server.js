1965f77a8b57f23ea0d48c83e4bc59c7a5b2a990
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-03-27 14:37:40 +0300

    Revert "Update server.js"
    This reverts commit bdcb035cc09aab0570cf0d65ccc5ce098feb14d5.

--- server.js
+++ server.js
@@ -41,8 +41,3 @@ app.use("/certification", require("./routers/certificationRouter"));
 app.use("/footer", require("./routers/footerRouter"));
 app.use("/mofa", require("./routers/mofaRouter"));
 app.use("/mm", require("./routers/mmRouter"));
-
-app.post("/save", async (req, res) => {
-  console.log.req.body.stringified;
-  res.send();
-});
